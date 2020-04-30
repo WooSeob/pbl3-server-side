c1f365e85dca9b587c764ea0865a119de5b44631
wooseob <bws96@naver.com>
2020-04-30 15:08:41 +0000

    merged dbtest.js and routes/user.js

--- dbtest.js
+++ dbtest.js
@@ -49,13 +49,9 @@ app.use('/auth', authRouter);
 app.use('/user', userRouter);
 
 //테스팅
-<<<<<<< HEAD
-// test.increasePoint('5e940910ecc5c844f02c2fda', 4000);
-=======
 //var test = require('./test');
 //test.increasePoint('5e940910ecc5c844f02c2fda', 4000);
 //test.delete();
->>>>>>> 323093a9623c71f1ac64d7c184d512944d3ba5e6
 
 app.get('/', function(req, res){
     res.send('<h1>Hello home page</h1>');
