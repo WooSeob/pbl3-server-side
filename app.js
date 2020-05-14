931ae12ace490cc99098007ae2c8e6a9951cbd9c
jinwoo1225 <jinwoo17962@gmail.com>
2020-05-14 22:22:07 +0900

    test commit

--- app.js
+++ app.js
@@ -1,43 +1,44 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+//app.js
+var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
 
-app.set('view engine', 'jade');
-app.set('views', './views');
+app.set("view engine", "jade");
+app.set("views", "./views");
 
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/template', function(req, res){
-    res.render('temp');
-})
-
-app.get('/form', function(req, res){
-    res.render('form');
-})
-
-app.post('/form_receiver', function(req, res){
-    var title = req.body.title;
-    var disc = req.body.description;
-    res.send(title + ', ' + disc);
-})
-
-app.get('/topic', function(req, res){
-    var topics = ['js is..', 'node is..', 'exp is...'];
-    
-    res.send(req.query.id);
-})
-
-app.get('/', function(req, res){
-    res.send('<h1>Hello home page</h1>');
+app.get("/template", function (req, res) {
+  res.render("temp");
 });
 
-app.get('/login', function(req, res){
-    res.send('Login please');
-})
+app.get("/form", function (req, res) {
+  res.render("form");
+});
 
-app.get('/dynamic', function(req, res){
-    var output = `
+app.post("/form_receiver", function (req, res) {
+  var title = req.body.title;
+  var disc = req.body.description;
+  res.send(title + ", " + disc);
+});
+
+app.get("/topic", function (req, res) {
+  var topics = ["js is..", "node is..", "exp is..."];
+
+  res.send(req.query.id);
+});
+
+app.get("/", function (req, res) {
+  res.send("<h1>Hello home page</h1>");
+});
+
+app.get("/login", function (req, res) {
+  res.send("Login please");
+});
+
+app.get("/dynamic", function (req, res) {
+  var output = `
     <!DOCTYPE html>
     <html>
         <head>
@@ -48,10 +49,9 @@ app.get('/dynamic', function(req, res){
         </body>
     </html>
     `;
-    res.send(output);
-})
+  res.send(output);
+});
 
-
-app.listen(3000, function(){
-    console.log('Connected 3000 port!');
+app.listen(3000, function () {
+  console.log("Connected 3000 port!");
 });
